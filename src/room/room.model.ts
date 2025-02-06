f15cc59dd0db26da89cb743a0125c02d7dfc85b0
legiorex <batyushev@gmail.com>
2025-02-06 09:16:11 +0700

    feat: refactor room model and integrate with Mongoose schema

--- src/room/room.model.ts
+++
@@ -1,7 +0,0 @@
-export class RoomModel {
-  _id: string;
-  number: number;
-  description: string;
-  capacity: number;
-  price: number;
-}
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoomController } from './room.controller';
+import { RoomModel, RoomSchema } from './model/room.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [RoomController],
+  imports: [MongooseModule.forFeature([{ name: RoomModel.name, schema: RoomSchema }])],
 })
 export class RoomModule {}
