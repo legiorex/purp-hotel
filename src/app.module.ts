32f4b46680ca82beaf0e6821c4041ecfffbb701b
Konstantin Batyushev <37956597+legiorex@users.noreply.github.com>
2025-02-15 09:40:28 +0300

    Revert "1 start"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomModule } from './room/room.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { mongooseConfig } from './config/mongoose.config';
-import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [
-    RoomModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: mongooseConfig,
-    }),
-    BookingModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/const.ts
+++
@@ -1,4 +0,0 @@
-export const ROOM_NOT_FOUND = 'room not found';
-export const ROOM_EXISTS = 'room already exists';
-export const ROOM_BOOKED = 'room is already booked';
-export const BOOKING_NOT_FOUND = 'booking not found';
